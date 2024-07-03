5f6b636879d7bbe8fa40bdd38db3801b4208798c
Jimmy Pangalinan <jimmi.pangalinan@metrocomjaddi.com>
2024-07-03 09:59:22 +0700

    update

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  let slideIndex = 0;
+  const slides = document.querySelectorAll('.banner-slide');
+  const totalSlides = slides.length;
+
+  function showSlides() {
+    slideIndex++;
+    if (slideIndex >= totalSlides) { slideIndex = 0; }
+    const offset = slideIndex * -100;
+    document.querySelector('.banner-container').style.transform = `translateX(${offset}%)`;
+    setTimeout(showSlides, 8000); // Change image every 8 seconds to match CSS animation
+  }
+
+  showSlides();
+});
